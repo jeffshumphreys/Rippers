af19838b2d80889b2b84fc83bec4f17beb55bbfa
Jeff Humphreys <47931319+jeffshumphreys@users.noreply.github.com>
2022-01-01 11:22:48 -0700

    Now that I'm home I can get the name of my machine, and fill in this magic string.

--- RipSSIS/SharedConstants.cs
+++ RipSSIS/SharedConstants.cs
@@ -2,14 +2,14 @@
 {
     public static class SharedConstants
     {
-        public const string       HOME_MACHINE = "DESKTOP-47LNE8R";
+        public const string       HOME_MACHINE = "DSKTP-HOME-JEFF";
         public const string CONTRACTOR_MACHINE = "ITT-JEFF-HUMPF2";
         // another might soon be: CLIENT_LOANER_MACHINE
 
-        public const string       HOME_ACCOUNT = @"DESKTOP-47LNE8R\jeffs";
+        public const string       HOME_ACCOUNT = @"DSKTP-HOME-JEFF\jeffs";
         public const string CONTRACTOR_ACCOUNT = @"INTIMETEC\jeff.humphreys";
 
-        public const string       HOME_PATH_TO_EXAMPLE_PACKAGES = @"D:\Users\jeffs\OneDrive - InTimeTec Visionsoft Pvt. Ltd.,\Documents\City of Boise\Packages\Examples";
+        public const string       HOME_PATH_TO_EXAMPLE_PACKAGES = @"D:\OneDrive - InTimeTec Visionsoft Pvt. Ltd.,\Documents\City of Boise\Packages\Examples";
         public const string CONTRACTOR_PATH_TO_EXAMPLE_PACKAGES = @"C:\Users\jeff.humphreys\OneDrive - InTimeTec Visionsoft Pvt. Ltd.,\Documents\City of Boise\Packages\Examples";
 
         public enum FolderOrFileEnum
